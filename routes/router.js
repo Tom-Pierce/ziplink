206a1d47495857f3aacffbbc7b3419ba7365552f
Tom Pierce <tompierce4@gmail.com>
2024-01-25 16:08:12 +0000

    swapped order of routes so that clicks could be accessed

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const urlController = require("../controllers/urlController");
 
-router.get("/:key", urlController.url_get);
-
 router.get("/clicks/:key", urlController.clicks_get);
+
+router.get("/:key", urlController.url_get);
 
 router.post("/url", urlController.url_post);
 
